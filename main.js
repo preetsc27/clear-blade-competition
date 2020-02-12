cfefe3f9ddabd7d71418e7a6cce726ad8493fffd
preetsc27 <preetsc27@gmail.com>
2020-02-12 00:08:27 -0600

    Update main.js

--- main.js
+++ main.js
@@ -17,6 +17,7 @@ function first(req,resp){
     var params = req.params;
     // getting the payload from the params
     var body = params.body
+    log(body)
 
     // replacing the ' with ". So that we can parse in json
     body = body.split("'").join('"')
@@ -45,6 +46,8 @@ function first(req,resp){
     // getting the collection refernce
     var col = ClearBlade.Collection({ collectionName: "preet_data" } );
     // saving the data
-    col.create(newPerson, callback);
-    resp.success("GOOD");
+    // Change: from @var newPerson to @var data. Before I gave a wrong var.
+    col.create(data, callback);
+    // Change: Remove resp from here send the resp from callback only 
+    // resp.success("GOOD");
 }
